ea3b7d607210f6c0e8493b2f89dbcaad6e3138d7
Karol <Karol@SZEROKILAPTOP>
2026-04-02 11:32:50 +0200

    Add project files.

---
+++ ztpai/MyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata;
+using ztpai.Models;
+
+namespace ztpai
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) 
+        { 
+        }
+    }
+
+}
